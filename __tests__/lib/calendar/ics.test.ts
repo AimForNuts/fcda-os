d6935261be955880e34faba5ebb26276c53064a9
AimForNuts <josepsloliveira@gmail.com>
2026-05-06 16:41:22 +0100

    feat: add map metadata to calendar feed

--- __tests__/lib/calendar/ics.test.ts
+++ __tests__/lib/calendar/ics.test.ts
@@ -12,10 +12,15 @@ describe('buildPlayerCalendarIcs', () => {
           id: 'game-1',
           date: '2026-05-07T20:30:00.000Z',
           location: 'Areosa',
+          mapLocation: 'Rua da Areosa, Porto',
+          mapsUrl: 'https://www.google.com/maps/search/?api=1&query=Areosa',
+          latitude: 41.123,
+          longitude: -8.456,
           playerTeamLabel: 'Team Blue',
         },
       ],
     })
+    const unfoldedIcs = ics.replace(/\r\n /g, '')
 
     expect(ics).toContain('BEGIN:VCALENDAR')
     expect(ics).toContain('X-WR-CALNAME:FCDA - Jose Oliveira')
@@ -23,7 +28,10 @@ describe('buildPlayerCalendarIcs', () => {
     expect(ics).toContain('DTSTART:20260507T203000Z')
     expect(ics).toContain('DTEND:20260507T213000Z')
     expect(ics).toContain('SUMMARY:FCDA - Team Blue')
+    expect(ics).toContain('LOCATION:Rua da Areosa\\, Porto')
+    expect(ics).toContain('GEO:41.123;-8.456')
     expect(ics).toContain('DESCRIPTION:Game time: 21:30 - 22:30\\nLocation: Areosa')
+    expect(unfoldedIcs).toContain('Map: https://www.google.com/maps/search/?api=1&query=Areosa')
     expect(ics).not.toContain('Player: Jose Oliveira')
     expect(ics).toContain('URL:https://fcda.example/matches/game-1')
   })
